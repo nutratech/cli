6da7a3c62321a1221a421794ae6cadb284b46344
Shane Jaroch <chown_tee@proton.me>
2022-07-12 15:19:23 -0400

    fix production issue, reorganize & shrink tarball
    exclude 2nd LICENSE in ntclient/ntsqlite from pypi
    
    move nt_init out of main services call
    
    bump version
    
    move changelog back up, reduce tarball size
    
    fix: NTSQLITE_DESTINATION is not defined
    
    bump to release version

--- ntclient/services/__init__.py
+++ ntclient/services/__init__.py
@@ -1,17 +1,11 @@
 """Services module, currently only home to SQL/persistence init method"""
 import os
 
-from ntclient import (
-    NTSQLITE_BUILDPATH,
-    NTSQLITE_DESTINATION,
-    NUTRA_DIR,
-    __db_target_nt__,
-)
+from ntclient import NUTRA_DIR
 from ntclient.ntsqlite.sql import build_ntsqlite
-from ntclient.persistence.sql.nt import nt_ver
+from ntclient.persistence.sql.nt import nt_init
 from ntclient.persistence.sql.usda import usda_init
 from ntclient.services import analyze, recipe, usda
-from ntclient.utils.exceptions import SqlInvalidVersionError
 
 
 def init(yes=False):
@@ -35,31 +29,7 @@ def init(yes=False):
 
     print("Nutra db         ", end="")
     build_ntsqlite()
-    # TODO: don't overwrite,
-    #  verbose toggle for download,
-    #  option to upgrade
-    if os.path.isfile(NTSQLITE_DESTINATION):
-        if nt_ver() != __db_target_nt__:
-            # TODO: hard requirement? raise error?
-            print(
-                "WARN: upgrades/downgrades not supported "
-                + "(actual: {0} vs. target: {1}), ".format(nt_ver(), __db_target_nt__)
-                + "please remove `~/.nutra/nt.sqlite3` file or ignore this warning"
-            )
-        print("..DONE!")
-        os.remove(NTSQLITE_BUILDPATH)  # clean up
-    else:
-        # TODO: is this logic (and these error messages) the best?
-        #  what if .isdir() == True ? Fails with stacktrace?
-        os.rename(NTSQLITE_BUILDPATH, NTSQLITE_DESTINATION)
-        if not nt_ver() == __db_target_nt__:
-            raise SqlInvalidVersionError(
-                "ERROR: nt target [{0}] mismatch actual [{1}], ".format(
-                    __db_target_nt__, nt_ver()
-                )
-                + ", please contact support or try again"
-            )
-        print("..DONE!")
+    nt_init()
 
     print("\nAll checks have passed!")
     return 0, True
