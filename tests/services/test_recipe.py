70fbb0b10ca4dfef067a56f9062cde2ea3092251
Shane Jaroch <chown_tee@proton.me>
2022-07-21 16:49:02 -0400

    unittest compliance (standard module, built-in)

--- tests/services/test_recipe.py
+++ tests/services/test_recipe.py
@@ -5,43 +5,46 @@ Created on Wed Jul 20 21:36:43 2022
 
 @author: shane
 """
+import unittest
+
 import pytest
 
 import ntclient.services.recipe as r
 
 
-def test_recipes_init():
-    """Checks the init function, which copies over default data (if not already)"""
-    exit_code, copy_count = r.recipes_init(_copy=False)
-    assert exit_code == 1
-    assert copy_count == 3
+class TestRecipe(unittest.TestCase):
+    """Tests the recipe service"""
 
-    exit_code, copy_count = r.recipes_init()
-    assert exit_code == 0
-    assert copy_count == 3
+    def test_recipes_init(self):
+        """Checks the init function, which copies over default data (if not already)"""
+        exit_code, copy_count = r.recipes_init(_copy=False)
+        assert exit_code == 1
+        assert copy_count == 3
 
+        exit_code, copy_count = r.recipes_init()
+        assert exit_code == 0
+        assert copy_count == 3
 
-def test_recipes_overview():
-    """Test type coercion and one-to-one input/output relationship"""
-    exit_code, _recipes = r.recipes_overview()
+    def test_recipes_overview(self):
+        """Test type coercion and one-to-one input/output relationship"""
+        exit_code, _recipes = r.recipes_overview()
 
-    assert exit_code == 1
-    assert isinstance(_recipes, list)
+        assert exit_code == 1
+        assert isinstance(_recipes, list)
 
-    exit_code, _recipes_overview = r.recipes_overview(
-        _recipes=(
-            (1, "test1", "tag1", 10, 75),
-            (2, "test2", "tag2", 15, 100),
-            (999, "test999", "tag999", 2, 5),
+        exit_code, _recipes_overview = r.recipes_overview(
+            _recipes=(
+                (1, "test1", "tag1", 10, 75),
+                (2, "test2", "tag2", 15, 100),
+                (999, "test999", "tag999", 2, 5),
+            )
         )
-    )
 
-    assert exit_code == 0
-    assert len(_recipes_overview) == 3
+        assert exit_code == 0
+        assert len(_recipes_overview) == 3
 
-
-def test_recipe_overview_throws_exc_for_negative_id():
-    """Raises index error if recipe int id is invalid"""
-    # TODO: should we be using guid / uuid instead of integer id?
-    with pytest.raises(IndexError):
-        r.recipe_overview(-12345)
+    def test_recipe_overview_throws_exc_for_negative_id(self):
+        """Raises index error if recipe int id is invalid"""
+        # TODO: should we be using guid / uuid instead of integer id?
+        with pytest.raises(IndexError):
+            r.recipe_overview(-12345)
--- tests/test_cli.py
+++ tests/test_cli.py
@@ -7,6 +7,7 @@ Created on Fri Jan 31 15:19:53 2020
 # pylint: disable=wrong-import-position
 import os
 import sys
+import unittest
 
 import pytest
 
@@ -39,260 +40,254 @@ TEST_HOME = os.path.dirname(os.path.abspath(__file__))
 arg_parser = build_argparser()
 
 
-def test_000_init():
-    """Tests the SQL/persistence init in real time"""
-    code, result = init(yes=True)
-    assert code == 0
-    assert result
+class TestCli(unittest.TestCase):
+    """Original one-stop-shop for testing"""
 
+    # pylint: disable=import-outside-toplevel
 
-def test_100_usda_sql_funcs():
-    """Performs cursory inspection (sanity checks) of usda.sqlite3 image"""
-    version = usda_ver()
-    assert version == __db_target_usda__
-    result = usda_funcs.sql_nutrients_details()
-    assert len(result[1]) == 186
+    def test_000_init(self):
+        """Tests the SQL/persistence init in real time"""
+        code, result = init(yes=True)
+        assert code == 0
+        assert result
 
-    result = usda_funcs.sql_servings({9050, 9052})
-    assert len(result) == 3
+    def test_100_usda_sql_funcs(self):
+        """Performs cursory inspection (sanity checks) of usda.sqlite3 image"""
+        version = usda_ver()
+        assert version == __db_target_usda__
+        result = usda_funcs.sql_nutrients_details()
+        assert len(result[1]) == 186
 
-    result = usda_funcs.sql_analyze_foods({23567, 23293})
-    assert len(result) == 188
+        result = usda_funcs.sql_servings({9050, 9052})
+        assert len(result) == 3
 
-    result = usda_funcs.sql_sort_foods(789)
-    assert len(result) == 415
-    # result = usda_funcs.sql_sort_foods(789, fdgrp_ids=[100])
-    # assert len(result) == 1
+        result = usda_funcs.sql_analyze_foods({23567, 23293})
+        assert len(result) == 188
 
-    result = usda_funcs.sql_sort_foods_by_kcal(789)
-    assert len(result) == 246
-    # result = usda_funcs.sql_sort_foods_by_kcal(789, fdgrp_ids=[1100])
-    # assert len(result) == 127
+        result = usda_funcs.sql_sort_foods(789)
+        assert len(result) == 415
+        # result = usda_funcs.sql_sort_foods(789, fdgrp_ids=[100])
+        # assert len(result) == 1
 
+        result = usda_funcs.sql_sort_foods_by_kcal(789)
+        assert len(result) == 246
+        # result = usda_funcs.sql_sort_foods_by_kcal(789, fdgrp_ids=[1100])
+        # assert len(result) == 127
 
-def test_200_nt_sql_funcs():
-    """Performs cursory inspection (sanity check) of nt.sqlite3 image"""
-    version = nt_ver()
-    assert version == __db_target_nt__
+    def test_200_nt_sql_funcs(self):
+        """Performs cursory inspection (sanity check) of nt.sqlite3 image"""
+        version = nt_ver()
+        assert version == __db_target_nt__
 
-    next_index = sql_nt_next_index("bf_eq")
-    assert next_index > 0
+        next_index = sql_nt_next_index("bf_eq")
+        assert next_index > 0
 
-    # TODO: add more tests; we used to comb over biometrics here
+        # TODO: add more tests; we used to comb over biometrics here
 
+    def test_300_argparser_debug_no_paging(self):
+        """Verifies the debug and no_paging flags are set"""
+        args = arg_parser.parse_args(args=["-d", "--no-pager"])
+        set_flags(args)
 
-def test_300_argparser_debug_no_paging():
-    """Verifies the debug and no_paging flags are set"""
-    args = arg_parser.parse_args(args=["-d", "--no-pager"])
-    set_flags(args)
+        assert args.debug is True
+        assert args.no_pager is True
 
-    assert args.debug is True
-    assert args.no_pager is True
+        from ntclient import DEBUG, PAGING  # pylint: disable=import-outside-toplevel
 
-    from ntclient import DEBUG, PAGING  # pylint: disable=import-outside-toplevel
+        assert DEBUG is True
+        assert PAGING is False
 
-    assert DEBUG is True
-    assert PAGING is False
+    def test_400_usda_argparser_funcs(self):
+        """Tests udsa functions in argparser.funcs (to varying degrees each)"""
+        # Init
+        args = arg_parser.parse_args(args=["init", "-y"])
+        assert args.yes is True
+        code, result = args.func(args=args)
+        assert code == 0
+        assert result
 
+        # Nutrients ( and `--no-pager` flag)
+        args = arg_parser.parse_args(args=["--no-pager", "nt"])
+        set_flags(args)  # unnecessary due to already happening, but hey
+        code, result = args.func()
+        assert code == 0
+        assert len(result) == 186
 
-def test_400_usda_argparser_funcs():
-    """Tests udsa functions in argparser.funcs (to varying degrees each)"""
-    # Init
-    args = arg_parser.parse_args(args=["init", "-y"])
-    assert args.yes is True
-    code, result = args.func(args=args)
-    assert code == 0
-    assert result
+        # Search
+        args = arg_parser.parse_args(args=["search", "grass", "beef"])
+        code, result = args.func(args)
+        assert code == 0
+        assert result
+        # Top 20 (beats injecting BUFFER_HT/DEFAULT_RESULT_LIMIT)
+        args = arg_parser.parse_args(args=["search", "grass", "beef", "-t", "20"])
+        code, result = args.func(args)
+        assert code == 0
+        assert len(result) == 20
+        assert result[0]["long_desc"] is not None
 
-    # Nutrients ( and `--no-pager` flag)
-    args = arg_parser.parse_args(args=["--no-pager", "nt"])
-    set_flags(args)  # unnecessary due to already happening, but hey
-    code, result = args.func()
-    assert code == 0
-    assert len(result) == 186
+        # Sort
+        args = arg_parser.parse_args(args=["sort", "789"])
+        code, result = args.func(args)
+        assert code == 0
+        assert result
+        # Top 20
+        args = arg_parser.parse_args(args=["sort", "789", "-t", "20"])
+        code, result = args.func(args)
+        assert code == 0
+        assert len(result) == 20
+        assert result[0][4] == "Capers, raw"
 
-    # Search
-    args = arg_parser.parse_args(args=["search", "grass", "beef"])
-    code, result = args.func(args)
-    assert code == 0
-    assert result
-    # Top 20 (beats injecting BUFFER_HT/DEFAULT_RESULT_LIMIT)
-    args = arg_parser.parse_args(args=["search", "grass", "beef", "-t", "20"])
-    code, result = args.func(args)
-    assert code == 0
-    assert len(result) == 20
-    assert result[0]["long_desc"] is not None
+        # Anl
+        args = arg_parser.parse_args(args=["anl", "9053"])
+        code, nutrients_rows, servings_rows = args.func(args)
+        assert code == 0
+        assert len(nutrients_rows[0]) == 30
+        assert len(servings_rows[0]) == 1
 
-    # Sort
-    args = arg_parser.parse_args(args=["sort", "789"])
-    code, result = args.func(args)
-    assert code == 0
-    assert result
-    # Top 20
-    args = arg_parser.parse_args(args=["sort", "789", "-t", "20"])
-    code, result = args.func(args)
-    assert code == 0
-    assert len(result) == 20
-    assert result[0][4] == "Capers, raw"
+        # Day
+        rda_csv_path = os.path.join(TEST_HOME, "resources", "rda", "dog-18lbs.csv")
+        day_csv_path = os.path.join(TEST_HOME, "resources", "day", "dog.csv")
+        args = arg_parser.parse_args(args=["day", "-r", rda_csv_path, day_csv_path])
+        code, result = args.func(args)
+        assert code == 0
+        assert result[0][213] == 1.295
+        assert len(result[0]) == 177
 
-    # Anl
-    args = arg_parser.parse_args(args=["anl", "9053"])
-    code, nutrients_rows, servings_rows = args.func(args)
-    assert code == 0
-    assert len(nutrients_rows[0]) == 30
-    assert len(servings_rows[0]) == 1
-
-    # Day
-    rda_csv_path = os.path.join(TEST_HOME, "resources", "rda", "dog-18lbs.csv")
-    day_csv_path = os.path.join(TEST_HOME, "resources", "day", "dog.csv")
-    args = arg_parser.parse_args(args=["day", "-r", rda_csv_path, day_csv_path])
-    code, result = args.func(args)
-    assert code == 0
-    assert result[0][213] == 1.295
-    assert len(result[0]) == 177
-
-
-def test_401_invalid_path_day_throws_error():
-    """Ensures invalid path throws exception in `day` subcommand"""
-    invalid_day_csv_path = os.path.join(
-        TEST_HOME, "resources", "day", "__NONEXISTENT_CSV_FILE__.csv"
-    )
-    with pytest.raises(SystemExit) as sys_exit:
-        arg_parser.parse_args(args=["day", invalid_day_csv_path])
-    assert sys_exit.value.code == 2
-
-    invalid_rda_csv_path = os.path.join(
-        TEST_HOME, "resources", "rda", "__NONEXISTENT_CSV_FILE__.csv"
-    )
-    with pytest.raises(SystemExit) as sys_exit:
-        arg_parser.parse_args(
-            args=["day", "-r", invalid_rda_csv_path, invalid_day_csv_path]
+    def test_401_invalid_path_day_throws_error(self):
+        """Ensures invalid path throws exception in `day` subcommand"""
+        invalid_day_csv_path = os.path.join(
+            TEST_HOME, "resources", "day", "__NONEXISTENT_CSV_FILE__.csv"
         )
-    assert sys_exit.value.code == 2
+        with pytest.raises(SystemExit) as sys_exit:
+            arg_parser.parse_args(args=["day", invalid_day_csv_path])
+        assert sys_exit.value.code == 2
 
+        invalid_rda_csv_path = os.path.join(
+            TEST_HOME, "resources", "rda", "__NONEXISTENT_CSV_FILE__.csv"
+        )
+        with pytest.raises(SystemExit) as sys_exit:
+            arg_parser.parse_args(
+                args=["day", "-r", invalid_rda_csv_path, invalid_day_csv_path]
+            )
+        assert sys_exit.value.code == 2
 
-def test_402_nt_argparser_funcs():
-    """Tests nt functions in argparser.funcs (to varying degrees each)"""
+    def test_402_nt_argparser_funcs(self):
+        """Tests nt functions in argparser.funcs (to varying degrees each)"""
 
+    def test_500_main_module(self):
+        """Tests execution of main() and __main__, in __main__.py"""
+        code = nt_main(args=["--no-pager", "nt"])
+        assert code == 0
 
-def test_500_main_module():
-    """Tests execution of main() and __main__, in __main__.py"""
-    code = nt_main(args=["--no-pager", "nt"])
-    assert code == 0
+        sys.argv = ["./nutra"]
+        code = nt_main()
+        assert code == 0
 
-    sys.argv = ["./nutra"]
-    code = nt_main()
-    assert code == 0
+        with pytest.raises(SystemExit) as system_exit:
+            nt_main(args=["-h"])
+        assert system_exit.value.code == 0
 
-    with pytest.raises(SystemExit) as system_exit:
-        nt_main(args=["-h"])
-    assert system_exit.value.code == 0
+        # __main__: if args_dict
+        code = nt_main(args=["anl", "9053", "-g", "80"])
+        assert code == 0
 
-    # __main__: if args_dict
-    code = nt_main(args=["anl", "9053", "-g", "80"])
-    assert code == 0
+    def test_600_sql_integrity_error__service_wip(self):
+        """Provokes IntegrityError in nt.sqlite3"""
 
+        # TODO: replace with non-biometric test
+        # from ntclient.services import biometrics
+        #
+        # args = arg_parser.parse_args(args=["-d", "bio", "log", "add", "12,12"])
+        # biometrics.input = (
+        #     lambda x: "y"
+        # )  # mocks input, could also pass `-y` flag or set yes=True
+        #
+        # with pytest.raises(sqlite3.IntegrityError) as integrity_error:
+        #     args.func(args)
+        # assert (
+        #     integrity_error.value.args[0]
+        #     == "NOT NULL constraint failed: biometric_log.profile_id"
+        # )
 
-def test_600_sql_integrity_error__service_wip():
-    """Provokes IntegrityError in nt.sqlite3"""
+    def test_700_build_ntsqlite_succeeds(self):
+        """Verifies the service level call for git submodule"""
+        try:
+            os.remove(NTSQLITE_BUILDPATH)
+        except FileNotFoundError:
+            pass
+        assert not os.path.exists(NTSQLITE_BUILDPATH)
 
-    # TODO: replace with non-biometric test
-    # from ntclient.services import biometrics # pylint: disable=import-outside-toplevel
-    #
-    # args = arg_parser.parse_args(args=["-d", "bio", "log", "add", "12,12"])
-    # biometrics.input = (
-    #     lambda x: "y"
-    # )  # mocks input, could also pass `-y` flag or set yes=True
-    #
-    # with pytest.raises(sqlite3.IntegrityError) as integrity_error:
-    #     args.func(args)
-    # assert (
-    #     integrity_error.value.args[0]
-    #     == "NOT NULL constraint failed: biometric_log.profile_id"
-    # )
-
-
-def test_700_build_ntsqlite_succeeds():
-    """Verifies the service level call for git submodule"""
-    try:
+        result = build_ntsqlite(verbose=True)
+        assert result is True
+        assert os.path.isfile(NTSQLITE_BUILDPATH)
         os.remove(NTSQLITE_BUILDPATH)
-    except FileNotFoundError:
-        pass
-    assert not os.path.exists(NTSQLITE_BUILDPATH)
 
-    result = build_ntsqlite(verbose=True)
-    assert result is True
-    assert os.path.isfile(NTSQLITE_BUILDPATH)
-    os.remove(NTSQLITE_BUILDPATH)
-
-
-@pytest.mark.skip(reason="Long-running, want to replace with more 'unit' style")
-def test_800_usda_upgrades_or_downgrades():
-    """Ensures the static usda.sqlite3 file can be upgraded/downgraded as needed"""
-    version = usda_ver()
-    major, minor, release = version.split(".")
-    new_release = str(int(release) + 1)
-    new_version = ".".join([major, minor, new_release])
-    _usda_sql(
-        "INSERT INTO version (version) VALUES (?)",
-        values=(new_version,),
-        version_check=False,
-    )
-
-    code, successful = init(yes=True)
-    assert code == 0
-    assert successful is True
-
-
-@pytest.mark.skip(reason="Long-running, want to replace with more 'unit' style")
-def test_801_sql_invalid_version_error_if_version_old():
-    """Throws base custom SqlException...
-    TODO: why lines still missing in `coverage` for __main__ ?"""
-    _usda_sql(
-        "DELETE FROM version WHERE version=?",
-        values=(__db_target_usda__,),
-        version_check=False,
-    )
-
-    with pytest.raises(SqlInvalidVersionError) as sql_invalid_version_error:
-        nt_main(["-d", "nt"])
-    assert sql_invalid_version_error is not None
-
-
-@pytest.mark.skip(reason="Long-running, want to replace with more 'unit' style")
-def test_802_usda_downloads_fresh_if_missing_or_deleted():
-    """Ensure download of usda.sqlite3.tar.xz, if usda.sqlite3 is missing"""
-    from ntclient.persistence.sql import usda  # pylint: disable=import-outside-toplevel
-
-    # TODO: similar for nt.sqlite3?
-    #  Define development standards.. rebuilding, deleting, preserving
-    #  remove whole `.nutra` in a special test?
-    try:
-        # TODO: export USDA_DB_PATH at package level,
-        #  don't pepper os.path.join() throughout code?
-        usda_path = os.path.join(NUTRA_HOME, USDA_DB_NAME)
-        os.remove(usda_path)
-    except (FileNotFoundError, PermissionError) as err:
-        # TODO: resolve PermissionError on Windows
-        print(repr(err))
+    @unittest.skip(reason="Long-running, want to replace with more 'unit' style")
+    def test_800_usda_upgrades_or_downgrades(self):
+        """Ensures the static usda.sqlite3 file can be upgraded/downgraded as needed"""
+        version = usda_ver()
+        major, minor, release = version.split(".")
+        new_release = str(int(release) + 1)
+        new_version = ".".join([major, minor, new_release])
         _usda_sql(
             "INSERT INTO version (version) VALUES (?)",
+            values=(new_version,),
+            version_check=False,
+        )
+
+        code, successful = init(yes=True)
+        assert code == 0
+        assert successful is True
+
+    @unittest.skip(reason="Long-running, want to replace with more 'unit' style")
+    def test_801_sql_invalid_version_error_if_version_old(self):
+        """Throws base custom SqlException...
+        TODO: why lines still missing in `coverage` for __main__ ?"""
+        _usda_sql(
+            "DELETE FROM version WHERE version=?",
             values=(__db_target_usda__,),
             version_check=False,
         )
-        pytest.xfail("PermissionError, are you using Microsoft Windows?")
 
-    usda.input = lambda x: "y"  # mocks input, could also pass `-y` flag or set yes=True
-    code, successful = init()
-    assert code == 0
-    assert successful is True
+        with pytest.raises(SqlInvalidVersionError) as sql_invalid_version_error:
+            nt_main(["-d", "nt"])
+        assert sql_invalid_version_error is not None
 
+    @unittest.skip(reason="Long-running, want to replace with more 'unit' style")
+    def test_802_usda_downloads_fresh_if_missing_or_deleted(self):
+        """Ensure download of usda.sqlite3.tar.xz, if usda.sqlite3 is missing"""
+        from ntclient.persistence.sql import usda
 
-def test_900_nut_rda_bar():
-    """Verifies colored/visual output is correctly generated"""
-    analysis = usda_funcs.sql_analyze_foods(food_ids={1001})
-    nutrients = usda_funcs.sql_nutrients_overview()
-    output = nutprogbar.nutprogbar(
-        food_amts={1001: 100}, food_analyses=analysis, nutrients=nutrients
-    )
-    assert output
+        # TODO: similar for nt.sqlite3?
+        #  Define development standards.. rebuilding, deleting, preserving
+        #  remove whole `.nutra` in a special test?
+        try:
+            # TODO: export USDA_DB_PATH at package level,
+            #  don't pepper os.path.join() throughout code?
+            usda_path = os.path.join(NUTRA_HOME, USDA_DB_NAME)
+            os.remove(usda_path)
+        except (FileNotFoundError, PermissionError) as err:
+            # TODO: resolve PermissionError on Windows
+            print(repr(err))
+            _usda_sql(
+                "INSERT INTO version (version) VALUES (?)",
+                values=(__db_target_usda__,),
+                version_check=False,
+            )
+            pytest.xfail("PermissionError, are you using Microsoft Windows?")
+
+        # mocks input, could also pass `-y` flag or set yes=True
+        usda.input = lambda x: "y"
+
+        code, successful = init()
+        assert code == 0
+        assert successful is True
+
+    def test_900_nut_rda_bar(self):
+        """Verifies colored/visual output is correctly generated"""
+        analysis = usda_funcs.sql_analyze_foods(food_ids={1001})
+        nutrients = usda_funcs.sql_nutrients_overview()
+        output = nutprogbar.nutprogbar(
+            food_amts={1001: 100}, food_analyses=analysis, nutrients=nutrients
+        )
+        assert output
