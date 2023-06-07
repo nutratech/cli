b0617b1933e3f1a0e6ac206267378ed1be4f4c87
Shane Jaroch <chown_tee@proton.me>
2023-06-07 07:11:26 -0400

    wip nnest, want to color code & tree view `anl`

--- ntclient/core/nnest.py
+++ ntclient/core/nnest.py
@@ -5,8 +5,27 @@ Created on Sat Aug 29 19:43:55 2020
 @author: shane
 """
 
+
+# pylint: disable=too-few-public-methods
+class Nutrient:
+    """Tracks properties of nutrients; used in the tree structure of nutrient groups"""
+
+    def __init__(self, nut_id: int, name: str, hidden: bool = False):
+        self.nut_id = nut_id
+        self.name = name
+        self.hidden = hidden
+
+
 nnest = {
-    "basics": ["Protein", "Carbs", "Fats", "Fiber", "Calories"],
+    # "basics": ["Protein", "Carbs", "Fats", "Fiber", "Calories"],
+    "basics": {
+        # 203: {"name": "Protein", "hidden": False},
+        203: Nutrient(203, "Protein"),
+        205: "Carbs",
+        204: "Fats",
+        291: "Fiber",
+        208: "Calories (kcal)",
+    },
     "macro_details": {"Carbs": {}, "Fat": {}},
     "micro_nutrients": {
         "Vitamins": {"Water-Soluble": {}, "Fat-Soluble": {}},
