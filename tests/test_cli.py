95cf615ee6f957e7e7d4f0f777b15508b7d52b74
Shane Jaroch <chown_tee@proton.me>
2024-02-25 15:33:11 -0500

    wip testing, bleh

--- tests/test_cli.py
+++ tests/test_cli.py
@@ -29,7 +29,7 @@ from ntclient.persistence.sql.nt.funcs import sql_nt_next_index
 from ntclient.persistence.sql.usda import funcs as usda_funcs
 from ntclient.persistence.sql.usda import sql as _usda_sql
 from ntclient.persistence.sql.usda import usda_ver
-from ntclient.services import init, usda
+from ntclient.services import bugs, init, usda
 from ntclient.services.recipe import RECIPE_HOME
 from ntclient.utils import CLI_CONFIG
 from ntclient.utils.exceptions import SqlInvalidVersionError
@@ -435,3 +435,9 @@ class TestCli(unittest.TestCase):
             _food_amts={1001: 100}, _food_analyses=analysis, _nutrients=nutrients
         )
         assert output
+
+    def test_1000_bugs(self):
+        """Tests the functions for listing and submitting bugs"""
+        bugs.list_bugs()
+        # TODO: this should be mocked
+        bugs.submit_bugs()
